93f7af008a3d0055d04fb8c3e955a8c09e989ce8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-09-03 13:37:35 +0100

    refactoring

--- pyoneten/util.py
+++ pyoneten/util.py
@@ -26,6 +26,10 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from base64 import b64decode, b64encode
+from Crypto.Cipher import AES
+from pkcs7 import PKCS7Encoder
+
 errorcodes = {
     '0': 'Success',
     '-1010': 'Invalid Public Key Length',
@@ -34,3 +38,28 @@ errorcodes = {
     '1002': 'Incorrect Request',
     '-1003': 'JSON formatting error',
 }
+
+class TpLinkCipher:
+
+    def __init__(self, b_arr, b_arr2):
+        self.iv = b_arr2
+        self.key = b_arr
+
+    def mime_encoder(to_encode):
+        encoded_list = list(b64encode(to_encode).decode("UTF-8"))
+        count = 0
+        for i in range(76, len(encoded_list), 76):
+            encoded_list.insert(i + count, '\r\n')
+            count += 1
+        return ''.join(encoded_list)
+
+    def encrypt(self, data):
+        data = PKCS7Encoder().encode(data)
+        cipher = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        encrypted = cipher.encrypt(data.encode("UTF-8"))
+        return TpLinkCipher.mime_encoder(encrypted).replace("\r\n","")
+
+    def decrypt(self, data):
+        aes = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+        pad_text = aes.decrypt(b64decode(data.encode("UTF-8"))).decode("UTF-8")
+        return PKCS7Encoder().decode(pad_text)
