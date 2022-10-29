9882f8146062b3a07ab16b67c72c278d275a6ab0
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2022-10-29 12:50:29 +0100

    mv

--- libiot/scripts/p110.py
+++ libiot/scripts/p110.py
@@ -27,7 +27,61 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 'Run given command on all configured Tapo P100/P110 plugs.'
-from ..cli import main_p110
+from . import initlogging
+from ..p110 import Identity, LoginParams, P110
+from ..util import Retry
+from argparse import ArgumentParser
+from aridity.config import Config, ConfigCtrl
+from concurrent.futures import ThreadPoolExecutor
+from contextlib import ExitStack
+from diapyr import DI, types
+from diapyr.util import invokeall
+import json, logging
+
+@types(this = Identity)
+def identityfactory():
+    return Identity.loadorcreate()
+
+@types(Config, Identity, LoginParams, this = P110)
+def p110factory(config, identity, loginparams):
+    return P110.loadorcreate(config, identity).Client(config, loginparams)
+
+class Command:
+
+    @types(Config, Retry, P110, str)
+    def __init__(self, config, retry, p110, name):
+        self.command = getattr(p110, config.command)
+        self.retry = retry
+        self.name = name
+
+    def __call__(self):
+        return self.name, self.retry(self.command)
+
+def main():
+    initlogging()
+    config = ConfigCtrl().loadappconfig(main, 'p110.arid')
+    parser = ArgumentParser()
+    parser.add_argument('--cron', action = 'store_true')
+    parser.add_argument('-f', action = 'store_true')
+    parser.add_argument('--fail', action = 'store_true')
+    parser.add_argument('--retry')
+    parser.add_argument('-v', action = 'store_true')
+    parser.add_argument('command')
+    parser.parse_args(namespace = config.cli)
+    logging.getLogger().setLevel(logging.DEBUG if config.verbose else logging.INFO)
+    with DI() as di, ExitStack() as stack, ThreadPoolExecutor() as e:
+        di.add(config)
+        di.add(identityfactory)
+        di.add(Retry)
+        di.add(LoginParams)
+        def entryfuture(name, conf):
+            plugdi = stack.enter_context(DI(di))
+            plugdi.add(name)
+            plugdi.add(conf)
+            plugdi.add(p110factory)
+            plugdi.add(Command)
+            return e.submit(plugdi(Command))
+        print(json.dumps(dict(invokeall([entryfuture(*item).result for item in -config.plug]))))
 
 if '__main__' == __name__:
-    main_p110()
+    main()
