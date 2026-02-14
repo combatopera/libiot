db8d03d03a1dd465a8baef1e87a0fa954d3ea380
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2026-02-14 10:56:11 +0000

    use splut to limit concurrency

--- libiot/scripts/mijia.py
+++ libiot/scripts/mijia.py
@@ -28,27 +28,32 @@
 
 'Get data from all configured Mijia thermometer/hygrometer 2 sensors.'
 from ..bluetoothctl import BluetoothShell
-from ..util import Retry
+from ..util import Retry, spawnfactory
 from argparse import ArgumentParser
 from aridity.config import Config, ConfigCtrl
 from concurrent.futures import ThreadPoolExecutor
 from diapyr import DI, types
 from foyndation import initlogging, invokeall
 from functools import partial
+from splut.actor import Spawn
 import json, logging
 
 class Script:
 
-    @types(Config, BluetoothShell, Retry, ThreadPoolExecutor)
-    def __init__(self, config, shell, retry, e):
-        self.exclude = set(config.exclude)
-        self.sensors = {name: s.address for name, s in -config.sensor}
+    @types(str, Config, BluetoothShell, Retry)
+    def __init__(self, name, s, shell, retry):
+        self.name = name
+        self.address = s.address
         self.shell = shell
         self.retry = retry
-        self.e = e
 
-    def run(self):
-        return dict(zip(self.sensors, invokeall([self.e.submit(self.retry, (lambda: None) if name in self.exclude else partial(self.shell.read_lywsd03mmc, address)).result for name, address in self.sensors.items()])))
+    def __call__(self):
+        return self.name, self.retry(partial(self.shell.read_lywsd03mmc, self.address))
+
+class W:
+
+    def run(self, task):
+        return task()
 
 def main():
     initlogging()
@@ -60,13 +65,21 @@ def main():
     parser.add_argument('-v', action = 'store_true')
     parser.parse_args(namespace = config.cli)
     logging.getLogger().setLevel(logging.DEBUG if config.verbose else logging.INFO)
-    with DI() as di, ThreadPoolExecutor() as e:
+    with DI() as di, ThreadPoolExecutor(100) as e:
         di.add(BluetoothShell)
         di.add(config)
         di.add(e)
+        di.add(spawnfactory)
         di.add(Retry)
-        di.add(Script)
-        print(json.dumps(di(Script).run()))
+        exclude = set(config.exclude)
+        for name, s in -config.sensor:
+            if name not in exclude:
+                subdi = DI(di)
+                subdi.add(name)
+                subdi.add(s)
+                subdi.add(Script)
+                subdi.join(Script)
+        print(json.dumps(dict(invokeall([a.run(s).wait for a in [di(Spawn)(*(W() for _ in range(4)))] for s in di.all(Script)]))))
 
 if '__main__' == __name__:
     main()
